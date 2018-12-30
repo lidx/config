9dfb9928aae0720579f4bac9c1885dea4ed941ed
lidx <lidongxu521521@126.com>
2018-12-30 17:22:35 +0800

    Initial commit

---
+++ src/main/java/com/demo/cloud/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.demo.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class DemoController {
+
+    @Value("${from}")
+    private String from;
+
+    @RequestMapping("/from")
+    public String from() {
+        return this.from;
+    }
+
+}
